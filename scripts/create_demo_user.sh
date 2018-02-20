474ac77d1fddb0b1b2cc0b700cc65118ffa242e1
Ben Galewsky <bengal1@illinois.ed>
2018-02-20 09:48:01 -0600

    Update create_demo_user script to use the new -p flag on ndsctl

--- scripts/create_demo_user.sh
+++ scripts/create_demo_user.sh
@@ -4,9 +4,5 @@ adminpassword=$(kubectl exec $apiserver cat password.txt)
 echo $apiserver
 echo $adminpassword
 kubectl cp scripts/account-register.json $apiserver:account-register.json
-echo "-----------Creating Shell in API Server for MANUAL STEP ---------------"
-echo "Login as admin with password: $adminpassword"
-echo "    ndslabsctl login admin"
-echo "and import default account as"
-echo "    ndslabsctl add account -f account-register.json"
-kubectl -it exec $apiserver bash
+kubectl exec $apiserver -- bash -c "ndslabsctl login admin -p $adminpassword && ndslabsctl add account -f account-register.json"
+grep password account-register.json
